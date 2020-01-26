70fbdcc323524804df94d277274ff3a484994d70
bradehrlich6 <60119950+bradehrlich6@users.noreply.github.com>
2020-01-26 16:22:07 -0500

    Add files via upload

--- You are Awesome!/You are Awesome!/ViewController.swift
+++ You are Awesome!/You are Awesome!/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    let messages = ["You are Great!", "Good Job!", "I like your shoes!", "Nice face!"]
+    var messageCount = 0
+    var imageCount = 0
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()// Do any additional setup after loading the view.
     }
-
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        messageLabel.text = messages[Int.random(in: 0...messages.count - 1)]
+        imageView.image = UIImage(named: "image\(Int.random(in:0...9))")
+    }
+    
 }
+    
+    
+
+
 
