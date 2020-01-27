c908640724a7031bf019f2e4158ef32141c96cec
bradehrlich6 <60119950+bradehrlich6@users.noreply.github.com>
2020-01-27 15:03:18 -0500

    Add files via upload
    2.14 Upload

--- You are Awesome!/You are Awesome!/ViewController.swift
+++ You are Awesome!/You are Awesome!/ViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import AVFoundation 
 
 class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
     
     let messages = ["You are Great!", "Good Job!", "I like your shoes!", "Nice face!"]
-    var messageCount = -1
-    var imageCount = -1
+    var messageCount = 0
+    var imageCount = 0
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()// Do any additional setup after loading the view.
@@ -31,8 +33,19 @@ class ViewController: UIViewController {
             newImageCount = Int.random(in:0...9)
         } while imageCount == newImageCount
         imageCount = newImageCount
-        
         imageView.image = UIImage(named: "image\(imageCount))")
+        
+         if let sound = NSDataAsset(name: "sound0") {
+        do{
+            try audioPlayer = AVAudioPlayer(data: sound.data)
+            audioPlayer.play()
+        } catch{
+            print("Error: \(error.localizedDescription) Could not intialize AudioPlaer Object")
+        }
+            
+        } else {
+            print("ERROR: Could not read data from file sound0")
+        }
     }
     
 }
